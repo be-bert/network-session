fe2cb02f6917eae0c15c9c5f912f20c5f0048a72
be-bert <bebert.apps@gmail.com>
2019-03-01 16:39:36 -0500

    Initial Commit

---
+++ NetworkSession/NetworkSession.h
@@ -0,0 +1,19 @@
+//
+//  NetworkSession.h
+//  NetworkSession
+//
+//  Created by Ben Lambert on 3/1/19.
+//  Copyright © 2019 CollectiveIdea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetworkSession.
+FOUNDATION_EXPORT double NetworkSessionVersionNumber;
+
+//! Project version string for NetworkSession.
+FOUNDATION_EXPORT const unsigned char NetworkSessionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkSession/PublicHeader.h>
+
+
